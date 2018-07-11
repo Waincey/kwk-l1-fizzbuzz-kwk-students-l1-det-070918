8feeba97e27a8a5b1742810734623bb1e9b6bf37
Waincey <waincey.chan@gmail.com>
2018-07-11 15:33:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
   if int % 3 && 5
   puts "Fizzbuzz"
-  elsif int % 5 == 0
+  elsif int % 5 == 1
   puts "Buzz"
-  elsif int % 3 == 0
+  elsif int % 3 == 1
     puts "Fizz"
 end
 end
